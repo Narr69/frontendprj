08a5b2f9add2ad4ab9f33afb17747586a4385447
MINOMINOMI <minomi7029@gmail.com>
2022-02-02 17:57:18 +0900

    fn_transition() -> PointerEvent Disable add

--- src/main/modules/type/temp.ts
+++ src/main/modules/type/temp.ts
@@ -2,12 +2,18 @@ function fn_transition(__id: string) {
     let seek = document.getElementById(__id);
     if ( seek.style.opacity == '1') {
         seek.style.opacity = '0';
+        seek.style.pointerEvents='none';
     }
     else {
         seek.style.opacity = '1';
+        seek.style.pointerEvents='';
     }
 }
 
 function fn_go_to(__id: string){
     document.getElementById(__id).scrollIntoView();
+}
+
+function fn_disable_anchor(__id: string){
+    let seek = document.getElementById(__id)
 }
--- src/main/temp.js
+++ src/main/temp.js
@@ -2,11 +2,16 @@ function fn_transition(__id) {
     let seek = document.getElementById(__id);
     if (seek.style.opacity == '1') {
         seek.style.opacity = '0';
+        seek.style.pointerEvents = 'none';
     }
     else {
         seek.style.opacity = '1';
+        seek.style.pointerEvents = '';
     }
 }
 function fn_go_to(__id) {
     document.getElementById(__id).scrollIntoView();
+}
+function fn_disable_anchor(__id) {
+    let seek = document.getElementById(__id);
 }
